1fa7e4207ee616b71b15e3b20b972556fef137a3
Michael Manning <michaelmanning390@gmail.com>
2018-06-06 16:20:21 -0700

    Some more frontend work. Dynamic linking to other page.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,14 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticleController extends AbstractController {
 
 	/**
-	 * @Route("/")
+	 * @Route("/", name="app_homepage")
 	 */
 	public function homepage() {
-		return new Response('Test first symfony page');
+		return $this->render('article/homepage.html.twig');
 	}
 
 	/**
-	 * @Route("/news/{slug}")
+	 * @Route("/news/{slug}", name="article_show")
 	 */
 	public function show($slug) {
 		$title = ucwords(str_replace('-', ' ', $slug));
